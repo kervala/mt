eb4da7f10b8a2b356a4a2ac4180099fe3a27aa73
Kervala <983521+kervala@users.noreply.github.com>
2012-04-14 17:40:28 +0200

    Changed: PCH files

---
+++ src/common.h
@@ -0,0 +1,26 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdint>
+#include <memory>
+#include <vector>
+#include <sstream>
+
+#if defined(_MSC_VER) && defined(_DEBUG)
+	#define _CRTDBG_MAP_ALLOC
+	#ifdef _malloca
+		#undef _malloca
+	#endif
+	#include <stdlib.h>
+	#include <crtdbg.h>
+	#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+	#ifdef realloc
+		#undef realloc
+	#endif
+#endif
+
+#endif
